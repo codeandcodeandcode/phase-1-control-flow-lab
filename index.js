b79fd47c9fbc1699d888370aa62aeff1c9a7c422
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-19 08:18:32 +0300

    Added code for city function

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function scuberGreetingForFeet(feet){
+
   let rv = "";
   if(feet<=400) rv= 'This is on me!';
   else if(feet>2000&&feet<2500) rv = 'I will gladly take your thirty bucks.';
   else rv = 'No can do.';
+  return rv;
+}
+
+console.log(scuberGreetingForFeet(199));
+
+function ternaryCheckCity(city){
+    
+  let rv;
+  city == "NYC" ? rv = 'Ok, sounds good.' : rv='No go.';
   return rv;
 }
 
