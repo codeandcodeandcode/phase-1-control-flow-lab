308fc5224b172db4d170c1854ed78171856348b2
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-19 08:17:08 +0300

    Added code for greetings function

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let rv = "";
+  if(feet<=400) rv= 'This is on me!';
+  else if(feet>2000&&feet<2500) rv = 'I will gladly take your thirty bucks.';
+  else rv = 'No can do.';
+  return rv;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
